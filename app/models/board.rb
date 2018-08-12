78545ae2564a39f13f544da1f7ec1b73417fa569
関純一 <>
2018-08-12 23:46:16 +0900

    バリデーションの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,5 @@
 class Board < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 10} # nameは未記入では投稿できない、10文字以上は記入できない
+  validates :title, presence: true, length: {maximum: 30}
+  validates :body, presence: true, length: {maximum: 1000}
 end
